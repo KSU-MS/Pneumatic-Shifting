307eb77e1bcddd77a1a39d4878933f0837340ec0
Luis-Rodriguez2 <Luisrodriguez533700@gmail.com>
2025-03-03 21:45:16 -0500

    Updated the main file and changed the old ones to txt as a guide.

--- include/main.hpp
+++ include/main.hpp
@@ -1,3 +1,4 @@
+#include <Arduino.h>
 //Output Pins
 #define LAUNCH_CONTROL 19
 #define NO_LIFT_SHIFT 20
@@ -10,3 +11,38 @@
 #define UP_SIGNAL 16
 #define DOWN_SIGNAL 17
 #define NEUTRAL_SIGNAL 18
+
+class stateMachine {
+    private:
+    enum states{ //Not really sure what to do with this - Luis
+        rest,
+        up_shift,
+        down_shift,
+        launch
+    };
+    states current_state = rest;//Or this - Luis
+    public:
+
+    void upShift(){
+        digitalWrite(NO_LIFT_SHIFT, HIGH);
+        digitalWrite(UP_PIN, HIGH);
+        delay(100); //Wait Function/Funky Pin read here
+    }
+    void downShift(){
+        digitalWrite(CLUTCH_PIN, HIGH);
+        digitalWrite(DOWN_PIN, HIGH);
+        delay(100);//Wait Function/Funky Pin read here
+    }
+    void launchState(){
+        digitalWrite(CLUTCH_PIN, HIGH);
+        digitalWrite(LAUNCH_CONTROL, HIGH);
+        delay(100);//Wait Function/Funcky Pin read here
+    }
+    void pinReset(){
+        digitalWrite(CLUTCH_PIN, LOW);
+        digitalWrite(UP_PIN, LOW);
+        digitalWrite(DOWN_PIN, LOW);
+        digitalWrite(NO_LIFT_SHIFT, LOW);
+        digitalWrite(LAUNCH_CONTROL, LOW);
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -2,104 +2,45 @@
 #include "main.hpp"
 uint16_t Paddle_Value;
 uint16_t Launch_Value;
+stateMachine main_state_machine;
 
-// This should also be a child function of the class ~Chance
-void PinReset(){ 
-    digitalWrite(UP_PIN, LOW);
-    digitalWrite(NO_LIFT_SHIFT, LOW);
-    digitalWrite(DOWN_PIN, LOW);
-    digitalWrite(CLUTCH_PIN, LOW);
-    digitalWrite(LAUNCH_CONTROL, LOW);
-}
-
-//TEMPORARY VARIABLES, WILL BE SET UP OR REMOVED ONCE SIGNAL STRENGTHS ARE FOUND
-int AcceptedLaunchValue = 1;
-int AcceptedPaddleValueUp = 1;
-int AcceptedPaddleValueDown = 1;
-int c = 0;
-
-// BTW this statement defining the class is something you would put in the main.hpp file ~Chance
-class state_machine{
-    private:
-    enum states{
-        rest,
-        up_shift,
-        down_shift,
-        launch
-    };
-    states current_state = rest;
-    
-    public:
-    void state_machine::nextState();
-};
-
-void state_machine::nextState(){
-    switch (this->current_state){
-    case rest:
-        Launch_Value = analogRead(LAUNCH_BUTTON_SIGNAL);
-        Paddle_Value = analogRead(PADDLE_SIGNAL);
-        if (Launch_Value = AcceptedLaunchValue){
-            current_state = launch;
-        }
-        else if (Paddle_Value == AcceptedPaddleValueUp){
-            current_state = up_shift;
-        }
-        else if (Paddle_Value == AcceptedPaddleValueDown){
-            current_state = down_shift;
-        }
-        else{ // You shouldn't have to state that it will stay in the same state but it's not a terrible habit ig ~Chance
-            current_state = rest;
-        }
-        break;
-    case launch:
-        digitalWrite(CLUTCH_PIN, HIGH);
-        while (Launch_Value == AcceptedLaunchValue){
-            serial_print("Waiting for button to be let go of");
-            Launch_Value = analogRead(LAUNCH_BUTTON_SIGNAL);
-        }
-        Paddle_Value = rest; // Are you sure you meant to set the Paddle Value here??? ~Chance
-        break;
-    case up_shift:
-        digitalWrite(NO_LIFT_SHIFT, HIGH);
-        digitalWrite(UP_PIN, HIGH);
-        //ADD WAIT FUNCTION DOWN TO UP PIN HEIGHT TIME (WILL WORRY ABOUT LATER)
-        while (Paddle_Value == AcceptedPaddleValueUp){
-            serial_print("Waiting for up paddle to be let go of");
-            Paddle_Value == analogRead(PADDLE_SIGNAL);
-        }
-        Paddle_Value = rest; // Think about how this evaluates line by line, what points the state machine back to the rest state? ~Chance
-        break;
-    case down_shift:
-        digitalWrite(CLUTCH_PIN, HIGH);
-        digitalWrite(DOWN_PIN, HIGH);
-        //ADD WAIT FUNCTION DOWN TO DOWN PIN HEIGHT TIME (WILL WORRY ABOUT LATER)
-        while (Paddle_Value == AcceptedPaddleValueDown){
-            serial_print("Waiting for down paddle to be let go of");
-            Paddle_Value == analogRead(PADDLE_SIGNAL);
-        }
-        Paddle_Value = rest; // Same thing here
-        break;
-    default: 
-        Paddle_Value = rest; // If this was the right variable, this would be a good catch, tho it should never happen ~Chance
-        break;
-    }
-};
-
-void setup(){
+void setup (){
     pinMode(UP_PIN, OUTPUT);
     pinMode(NO_LIFT_SHIFT, OUTPUT);
     pinMode(LAUNCH_CONTROL, OUTPUT);
     pinMode(DOWN_PIN, OUTPUT);
     pinMode(CLUTCH_PIN, OUTPUT);
-}   
+    main_state_machine.pinReset();//Sets all pins to low
+}
 
-void loop(){
-    state_machine(); // This is not how you define and refrence classes ~Chance
-    PinReset(); //end of loop
-};
+void loop (){
+    // Button Reads
+    Paddle_Value = analogRead(PADDLE_SIGNAL);
+    Launch_Value = analogRead(LAUNCH_BUTTON_SIGNAL);
 
-// So how do you want to go about this next? I can show you how I would model the system, think about
-// structure, and actually write all the code in front of you in one sitting or I can continue to try 
-// and give you hints about how state machines are used and why. Up to whatever you think would be 
-// better for you to learn. Its my first time trying to teach to this extent so feedback would also
-// be cool if there is something you think I am doing wrong. ~Chance
+    if (Launch_Value == 1){
+        main_state_machine.launchState();
+        while (Launch_Value == 1){
+            Launch_Value = analogRead(LAUNCH_BUTTON_SIGNAL);
+            delay(100);//Wait Function/Funcky Pin read here
+        }
+    }
+    else if (Paddle_Value == 1/*Paddled value to be decided later, upshift */){
+        main_state_machine.upShift();
+        while (Paddle_Value == 1){
+            Paddle_Value = analogRead(PADDLE_SIGNAL);
+            delay(100);//Wait Function/Funcky Pin read here
+        }
+    }
+    else if (Paddle_Value == 2/*Paddled value to be decided later, downshift */){
+        main_state_machine.downShift();
+        while (Paddle_Value == 2){
+            Paddle_Value = analogRead(PADDLE_SIGNAL);
+            delay(100);//Wait Function/Funcky Pin read here
+        }
+    }
+    else{
+        main_state_machine.pinReset();//Safety Guard
+    }
+    main_state_machine.pinReset();
+}
