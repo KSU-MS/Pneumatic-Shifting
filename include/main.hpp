238ec7555bda5399cafe1ae6bde33861f6d68f52
Luis-Rodriguez2 <Luisrodriguez533700@gmail.com>
2025-03-07 22:56:12 -0500

    Updated To Include Readable Paddle Volatages

--- include/main.hpp
+++ include/main.hpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+uint16_t Paddle_Value;
+uint16_t Launch_Value;
 //Output Pins
 #define LAUNCH_CONTROL 19
 #define NO_LIFT_SHIFT 20
@@ -12,36 +14,42 @@
 #define DOWN_SIGNAL 17
 #define NEUTRAL_SIGNAL 18
 
-class stateMachine {
-    private:
-    enum states{ //Not really sure what to do with this - Luis
-        rest,
-        up_shift,
-        down_shift,
-        launch
-    };
-    states current_state = rest;//Or this - Luis
+class PS3 {
+
     public:
 
     void upShift(){
         digitalWrite(NO_LIFT_SHIFT, HIGH);
         digitalWrite(UP_PIN, HIGH);
-        delay(100); //Wait Function/Funky Pin read here
+        delay(100); //Delay to allow up cylinder to reach height
     }
     void downShift(){
         digitalWrite(CLUTCH_PIN, HIGH);
+        digitalWrite(LAUNCH_CONTROL, HIGH);
+        delay(100);//Delay to allow clutch pin to reach height
         digitalWrite(DOWN_PIN, HIGH);
-        delay(100);//Wait Function/Funky Pin read here
+        delay(100);//Delay to allow down cylinder to reach height
+        digitalWrite(CLUTCH_PIN, LOW);
+        delay(100);//Delay to allow clutch to drop
+        digitalWrite(LAUNCH_CONTROL, LOW);
     }
-    void launchState(){
+    void launchStateActive(){
         digitalWrite(CLUTCH_PIN, HIGH);
         digitalWrite(LAUNCH_CONTROL, HIGH);
-        delay(100);//Wait Function/Funcky Pin read here
+        delay(100);//Delay to allow clutch pin to reach height
     }
-    void pinReset(){
+    void launchStateDisengage(){
+        digitalWrite(LAUNCH_CONTROL, LOW);
+        delay(2000);//Delay for the bleeder Valve to slowly Release Preasure
         digitalWrite(CLUTCH_PIN, LOW);
+        delay(100);//Delay to allow clutch to drop
+
+    }
+
+    void pinReset(){
         digitalWrite(UP_PIN, LOW);
         digitalWrite(DOWN_PIN, LOW);
+        digitalWrite(CLUTCH_PIN, LOW);
         digitalWrite(NO_LIFT_SHIFT, LOW);
         digitalWrite(LAUNCH_CONTROL, LOW);
     }
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
-#include <Arduino.h>
 #include "main.hpp"
-uint16_t Paddle_Value;
-uint16_t Launch_Value;
-stateMachine main_state_machine;
+PS3 pneumaticMain;
 
 void setup (){
     pinMode(UP_PIN, OUTPUT);
@@ -10,7 +7,7 @@ void setup (){
     pinMode(LAUNCH_CONTROL, OUTPUT);
     pinMode(DOWN_PIN, OUTPUT);
     pinMode(CLUTCH_PIN, OUTPUT);
-    main_state_machine.pinReset();//Sets all pins to low
+    pneumaticMain.pinReset();//Sets all pins to low
 }
 
 void loop (){
@@ -18,29 +15,39 @@ void loop (){
     Paddle_Value = analogRead(PADDLE_SIGNAL);
     Launch_Value = analogRead(LAUNCH_BUTTON_SIGNAL);
 
-    if (Launch_Value == 1){
-        main_state_machine.launchState();
-        while (Launch_Value == 1){
+    if (Launch_Value > 115 && Launch_Value < 126 /*Calculated Launch Button Voltages*/){
+        pneumaticMain.launchStateActive();
+        while (Launch_Value > 115 && Launch_Value < 126){
             Launch_Value = analogRead(LAUNCH_BUTTON_SIGNAL);
-            delay(100);//Wait Function/Funcky Pin read here
+            serial_print("Launch Button is being held");
+            delay(1);//Waiting to prevent sending too many messages
         }
+        serial_print("Launch Button Released");
+        pneumaticMain.launchStateDisengage();
     }
-    else if (Paddle_Value == 1/*Paddled value to be decided later, upshift */){
-        main_state_machine.upShift();
-        while (Paddle_Value == 1){
+    else if (Paddle_Value > 108 && Paddle_Value < 117 /*Calculated Right Paddle Voltages*/){
+        pneumaticMain.upShift();
+        pneumaticMain.pinReset();//Resets the pins again to prevent constant up shifting
+        while (Paddle_Value > 108 && Paddle_Value < 117){
             Paddle_Value = analogRead(PADDLE_SIGNAL);
-            delay(100);//Wait Function/Funcky Pin read here
+            serial_print("Right Paddle is being held");
+            delay(1);//Waiting to prevent sending too many messages
         }
+        serial_print("Right Paddle Released");
     }
-    else if (Paddle_Value == 2/*Paddled value to be decided later, downshift */){
-        main_state_machine.downShift();
-        while (Paddle_Value == 2){
+    else if (Paddle_Value > 96 && Paddle_Value < 106 /*Calculated Left Paddle Voltages*/){
+        pneumaticMain.downShift();
+        
+        pneumaticMain.pinReset();//Resets the pins again to prevent constant down shifting
+        while (Paddle_Value > 96 && Paddle_Value < 106){
             Paddle_Value = analogRead(PADDLE_SIGNAL);
-            delay(100);//Wait Function/Funcky Pin read here
+            serial_print("Left Paddle is being held");
+            delay(1);//Waiting to prevent sending too many messages
         }
+        serial_print("Left Paddle Released");
     }
     else{
-        main_state_machine.pinReset();//Safety Guard
+        pneumaticMain.pinReset();//Safety Guard
     }
-    main_state_machine.pinReset();
+    pneumaticMain.pinReset();//Extra Safety Guard
 }
